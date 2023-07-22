2b9a88c97f1205e0011ff5e1815cd5ef69563ca6
Darhniel <81332139+Darhniel@users.noreply.github.com>
2023-07-22 03:24:30 +0100

    update PostPage.js

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -38,7 +38,7 @@ export default function PostPage() {
                 </div>
             )}
             <div className="image">
-                <img src={`http://localhost:4000/${postInfo.image}`} alt="" />
+                <img src={`https://sample-blog-api.vercel.app/${postInfo.image}`} alt="" />
             </div>
             
             <div className="content" dangerouslySetInnerHTML={{__html: postInfo.content}}/>
