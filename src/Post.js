7f2f361dcf5085ff6d799483ea34e04ed0593154
Darhniel <81332139+Darhniel@users.noreply.github.com>
2023-07-22 01:02:03 +0100

    update Post.js

--- src/Post.js
+++ src/Post.js
@@ -7,7 +7,7 @@ export default function Post({_id, title, summary, image, createdAt, author}) {
         <div className="post">
             <div className="image">
                 <Link to={`/post/${_id}`}>
-                    <img src={`http://localhost:4000/${image}`} alt="post" />
+                    <img src={`https://sample-blog-api.vercel.app/${image}`} alt="post" />
                 </Link>
             </div>
             <div className="text">
