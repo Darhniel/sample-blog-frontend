7364c998ac02b61220131b7d54931222f1dee179
Darhniel <81332139+Darhniel@users.noreply.github.com>
2023-07-22 03:12:21 +0100

    Update CreatePost.js

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -33,7 +33,7 @@ export default function CreatePost() {
         data.set('file', files[0]);
         
         e.preventDefault();
-        const response = await fetch("http://localhost:4000/post", {
+        const response = await fetch("https://sample-blog-api.vercel.app/post", {
             method: 'POST',
             body: data,
             credentials: "include"
